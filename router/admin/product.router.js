858219a848e5842d6e6272de9d5512407d0373ea
nguyen2003kd <vudinhnguy2003@gmail.com>
2025-04-26 21:38:15 +0700

    upload image cloud

--- router/admin/product.router.js
+++ router/admin/product.router.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 const productController = require('../../controllers/admin/product.controller.js');
 const productValidate=require('../../validate/admin/product-create.js')
+const mulerCloud = require('../../middlewares/muler-cloud.js')
 //thư viện multer để tải ảnh
 const multer  = require('multer')
-const multerEditor= require('../../helpers/multer-editor.js')
-const upload = multer({storage:multerEditor()})
+// const multerEditor= require('../../helpers/multer-editor.js')
+const upload = multer()
 router.get("/",productController.index);
 router.patch("/change-status/:status/:id",productController.changeStatus);
 router.patch("/mutil-change-status",productController.mutillChangeStatus);
 router.patch("/change-deleted/:id",productController.deleted);
 router.get("/create",productController.create)
-router.post("/create",upload.single('thumbnail'),productValidate.createPost,productController.createPost)
+router.post("/create",upload.single('thumbnail'),mulerCloud.mulerCloud,productValidate.createPost,productController.createPost)
 router.get("/edit/:id",productController.edit)
-router.patch("/edit/:id",upload.single('thumbnail'),productValidate.createPost,productController.editPost)
+router.patch("/edit/:id",upload.single('thumbnail'),mulerCloud.mulerCloud,productValidate.createPost,productController.editPost)
 router.get("/detail/:id",productController.detail)
 module.exports = router;
